600ed7404a8a5d0f146099727b06a1562b7c53c8
eric646464 <ericmeeru@yahoo.co.jp>
2020-07-13 21:14:47 +0900

    選択したカラオケ動画を表示させる専用ページを作って見るのもいいかも

--- src/pages/ViewKaraokePage.jsx
+++ src/pages/ViewKaraokePage.jsx
@@ -1,2 +1,27 @@
 import react from 'react';
 
+const ViewKaraokePage = () => {
+
+    const containerCSS = {
+        position: "relative",
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        border: "red 5px solid",
+        margin: "10px 0",
+        borderRadius: "10px",
+        backgroundColor: "orange",
+        height: "100vh",
+        alignItems: "center",
+        padding: "10px",
+        color: "white",
+    }
+
+    return(
+        <div style={containerCSS}>
+            
+        </div>
+    );
+}
+
+export default ViewKaraokePage
