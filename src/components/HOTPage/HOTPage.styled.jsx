d1be83696beaa8d752ed6f5b599c52f30c4f9949
iida nozomi <nozomi@mbp.local>
2020-07-30 12:04:57 +0900

    youtube responsive

---
+++ src/components/HOTPage/HOTPage.styled.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const StyledComponent = styled.div`
+  .youtube {
+    position: relative;
+    width: 100%;
+    padding-top: 56.25%;
+  }
+  .youtube iframe {
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 100% !important;
+    height: 100% !important;
+  }
+`
